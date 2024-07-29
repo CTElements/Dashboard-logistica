4aecad0a1a21bfd7e1e7050a7e80fb8ce2a34332
CTElements <marketing@elements.com.br>
2024-07-29 17:28:28 -0300

    update vapt

--- controllers/vapt.js
+++ controllers/vapt.js
@@ -1,47 +1,30 @@
 
 const axios = require('axios');
 const qs = require('qs');
+
 module.exports = {
     async vapt(req, res){
 
        try {
-           const loginUrl = 'http://localhost/vector-api/app/vapt/login';
-           const getHeadsUrl = 'http://localhost/vector-api/app/vapt/get-order-elements';
-
+           const loginUrl = 'https://vector.log.br/api/app/vapt/login';
+           const getHeadsUrl = 'https://vector.log.br/api/app/vapt/get-order-elements';
            const loginData = {
                username: '759',
                password: 'X0i#7W4}IPSWjHQqc+04'
            };
-
-           axios.post(loginUrl, qs.stringify(loginData), {
+           const response = await axios.post(loginUrl, qs.stringify(loginData), {
                headers: {
                    'Content-Type': 'application/x-www-form-urlencoded'
                }
            })
-               .then(response => {
-                   return res.status(200).json(response.data)
-                  
-                   const responseData = response.data;
-
-                   if (responseData.token) {
-                       const token = responseData.token;
-                       console.log("Token JWT:", token);
-                       return axios.get(getHeadsUrl, {
-                           headers: {
-                               Authorization: `Bearer ${token}`
-                           }
-                       });
-                   } else {
-                       throw new Error('Resposta inesperada');
-                   }
-               })
-               .then(response => {
-                   console.log("Resposta do servidor:", response.data);
-               })
-               .catch(error => {
-                   res.status(500).json(error.cause)
-               });
-
+           const token = response.data.token
+           const responseData = await axios.get(getHeadsUrl, {
+               headers: {
+                   Authorization: `Bearer ${token}`
+               }
+           });
+           res.status(200).json(responseData.data)
+        
        } catch (error) {
          res.status(500).json(error)
        }
